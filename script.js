a56d2adb8eacedaf39065e40f823cf73ac24dd87
sebastianmontandon <sam171990@gmail.com>
2023-03-14 22:58:14 -0300

    ToDo List maker app(practice)

---
+++ script.js
@@ -0,0 +1,44 @@
+// Date Info
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+  const date = new Date();
+  dateNumber.textContent = date.toLocaleString('es', { day: 'numeric'});
+  dateText.textContent = date.toLocaleString('es', { weekday: 'long'});
+  dateMonth.textContent = date.toLocaleString('es', { month: 'short'});
+  dateYear.textContent = date.toLocaleString('es', { year: 'numeric'});
+};
+
+const addNewTask = event => {
+  event.preventDefault();
+  const { value } = event.target.taskText;
+  if (!value) return;
+  const task = document.createElement('div');
+  task.classList.add('task', 'roundedBorder');
+  task.addEventListener('click', changeTaskState);
+  task.textContent = value;
+  tasksContainer.prepend(task);
+  event.target.reset();
+};
+
+const changeTaskState = event => {
+  event.target.classList.toggle('done');
+  order().forEach(ele => tasksContainer.appendChild(ele));
+};
+
+const order = () => {
+  const done = [];
+  const toDo = [];
+  tasksContainer.childNodes.forEach( ele => {
+    ele.classList.contains('done') ? done.push(ele) : toDo.push(ele)
+  })
+  return [...toDo, ...done];
+};
+
+setDate();
